bdac2c6f2620e4e6e4935c94b0e0c39abb55c537
Subratam6296 <padraramesh420@gmail.com>
2023-11-25 18:48:25 +0530

    redwood: parts: Fix per app Thermal Profile as per FKM
    * thermal-extreme.conf = 2.
    * thermal-pubg.conf = 13.
    
    Signed-off-by: Subratam6296 <padraramesh420@gmail.com>
    Signed-off-by: Choirul Umam <bavvtm@outlook.com>

--- parts/src/org/lineageos/settings/thermal/ThermalUtils.java
+++ parts/src/org/lineageos/settings/thermal/ThermalUtils.java
@@ -43,11 +43,11 @@ public final class ThermalUtils {
     protected static final int STATE_STREAMING = 6;
 
     private static final String THERMAL_STATE_DEFAULT = "0";
-    private static final String THERMAL_STATE_BENCHMARK = "10";
+    private static final String THERMAL_STATE_BENCHMARK = "2";
     private static final String THERMAL_STATE_BROWSER = "11";
     private static final String THERMAL_STATE_CAMERA = "12";
     private static final String THERMAL_STATE_DIALER = "8";
-    private static final String THERMAL_STATE_GAMING = "9";
+    private static final String THERMAL_STATE_GAMING = "13";
     private static final String THERMAL_STATE_STREAMING = "14";
 
     private static final String THERMAL_BENCHMARK = "thermal.benchmark=";
